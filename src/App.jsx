38da2ac93b4cf28edfead23a2dbec7d31c243a25
romeoscript <romeobourne211@gmail.com>
2023-04-26 20:27:20 +0100

    feat;header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/draftlogo.jpeg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 import { Route, Routes } from 'react-router-dom'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/Signup'
