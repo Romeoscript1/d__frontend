a17650d1e96537230e8c0863f24bf317ab3ef8e0
romeoscript <romeobourne211@gmail.com>
2023-05-13 03:37:00 +0100

    feat:done

--- src/components/contactDetails.jsx
+++ src/components/contactDetails.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import FAQ from "./Faq";
 
 const contactDetails = () => {
   return (
-    <div className='w-3/5 rounded-md'> contactDetails</div>
-  )
-}
+    <>
+      <div className="w-[70%] rounded-md bg-[#571ce0] m-auto mb-[4rem] p-[2rem]  text-center">
+        <h1 className="text-5xl font-bold">Get in touch with us</h1>
+        <p className="my-[2rem] w-3/5 m-auto text-xl">
+          Fill up the form and our team will get back to you within 24 hours
+        </p>
 
-export default contactDetails
+        <form action="">
+          <div className="flex justify-between gap-[2%]">
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              className="rounded-md w-[49%] bg-white"
+            />
+            <TextField
+              id="filled-basic"
+              label="E-mail"
+              className="rounded-md w-[49%] bg-white"
+            />
+          </div>
+          <div className="flex justify-between gap-[2%] my-[1rem]">
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              className="rounded-md w-[49%] bg-white"
+            />
+            <TextField
+              id="filled-basic"
+              label="E-mail"
+              className="rounded-md w-[49%] bg-white"
+            />
+          </div>
+          <TextField
+            id="outlined"
+            label="Multiline"
+            multiline
+            rows={6}
+            variant="outlined"
+            className="w-full rounded-md bg-white my-[1rem]"
+          />
+
+          <button className="py-[0.5rem] px-[3rem] border-[1px] border-white rounded-md my-[1rem] mx-auto">
+            submit
+          </button>
+        </form>
+      </div>
+      <FAQ />
+    </>
+  );
+};
+
+export default contactDetails;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import Banner from '../components/Banner'
-import contactImg from '../assets/contact-illus.png'
-import ContactDetails from '../components/contactDetails'
+import React from "react";
+import Layout from "../components/Layout";
+import Banner from "../components/Banner";
+import contactImg from "../assets/contact-illus.png";
+import ContactDetails from "../components/contactDetails";
 
 const Contact = () => {
   return (
     <div>
-        <Layout>
+      <Layout>
         <Banner className="contact">
           <div className="text-5xl font-bold text-white h-full z-20 flex items-center p-[4rem] justify-right relative">
             <p>contact us </p>
-            <img src={contactImg} alt=""  className='absolute right-0 h-[90%] w-3/5 object-contain'/>
+            <img
+              src={contactImg}
+              alt=""
+              className="absolute right-0 h-[90%] w-3/5 object-contain"
+            />
           </div>
         </Banner>
 
-    <ContactDetails/>
-        </Layout>
+        <ContactDetails />
+      </Layout>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
