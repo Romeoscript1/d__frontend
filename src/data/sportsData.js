ac33f4f16c49679f436cc16bd1556a3f2f9b418f
temple69 <utemple169@gmail.com>
2023-06-10 08:07:15 +0100

    array of objects for links and mock up sportsdata

---
+++ src/data/sportsData.js
@@ -0,0 +1,176 @@
+export const sportsLeagues = [
+  {
+    id: 0,
+    sport: "Discover",
+    link: "",
+  },
+  {
+    id: 1,
+    sport: "NFL",
+    link: "nfl",
+  },
+  {
+    id: 2,
+    sport: "NBA",
+    link: "nba",
+  },
+  {
+    id: 3,
+    sport: "NHL",
+    link: "nhl",
+  },
+  {
+    id: 4,
+    sport: "EPL",
+    link: "epl",
+  },
+  {
+    id: 5,
+    sport: "PGA",
+    link: "pga",
+  },
+  {
+    id: 6,
+    sport: "CBB",
+    link: "cbb",
+  },
+  {
+    id: 7,
+    sport: "More",
+    link: "",
+  },
+];
+export const navLinks = [
+  {
+    id: 1,
+    name: "FANTASY",
+    link: "Fantasy",
+  },
+  {
+    id: 20,
+    name: "SPORTSBOOK",
+    link: "Sportsbook",
+  },
+  {
+    id: 3,
+    name: "HORSE_RACING",
+    link: "Horse_Racing",
+  },
+  {
+    id: 4,
+    name: "FACEOFF",
+    link: "Faceoff",
+  },
+];
+export const secondaryLinks = [
+  {
+    id: 1,
+    name: "Lobby",
+    link: "Lobby",
+  },
+  {
+    id: 2,
+    name: "Upcoming",
+    link: "Upcoming",
+  },
+  {
+    id: 3,
+    name: "Live",
+    link: "Live",
+  },
+  {
+    id: 4,
+    name: "History",
+    link: "History",
+  },
+  {
+    id: 5,
+    name: "Friends",
+    link: "Friends",
+  },
+  {
+    id: 6,
+    name: "Research",
+    link: "Research",
+  },
+];
+export const entries = [
+  {
+    id: 1,
+    name: "Tournament",
+    link: "tournament",
+  },
+  {
+    id: 2,
+    name: "Multipliers",
+    link: "muiltipliers",
+  },
+  {
+    id: 3,
+    name: "Head to Head",
+    link: "head-to-head",
+  },
+  {
+    id: 4,
+    name: "3-100 Players",
+    link: "players",
+  },
+  {
+    id: 5,
+    name: "50/50",
+    link: "equal",
+  },
+];
+export const mockDataEpl = [
+  {
+    id: 1,
+    value: 622,
+    valueString: "622",
+    title: "$8k Thu EPL Wonder Strike($2k to 1st)",
+    price: "8,000",
+    maxValue: 1716,
+    maxValueString: "1,716",
+    entryFee: "5.55",
+  },
+  {
+    id: 2,
+    value: 25,
+    valueString: "25",
+    title: "$1k Thu EPL Golden Goal($250 to lst)",
+    price: "1,000",
+    maxValue: 36,
+    maxValueString: "36",
+    entryFee: "33 Single",
+  },
+
+  {
+    id: 3,
+    value: 380,
+    valueString: "380",
+    title: "$400 Thu EPL Equalizer(Single Entry)",
+    price: "400",
+    maxValue: 476,
+    maxValueString: "1476",
+    entryFee: "1 single",
+  },
+  {
+    id: 4,
+    value: 1590,
+    valueString: "1,590",
+    title: "$8k Thu EPL Toke Poe($0.25 to eneter)",
+    price: "400",
+    maxValue: 1917,
+    maxValueString: "1,916",
+    entryFee: 0.25,
+  },
+  {
+    id: 5,
+    value: 4445,
+    valueString: "4,445",
+    title: "$200  EPL Pinny",
+    price: "200",
+    maxValue: 4790,
+    maxValueString: "4,790",
+    entryFee: 0.05,
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,10 @@ import Join from "../components/Join";
 import Layout from "../components/Layout";
 import TestimonialCard from "../components/TestimonialCard";
 import Wrapper from "../components/Wrapper";
+import Welcome from "../components/Welcome";
+import Dashboard from "../components/Dashboard";
+import LeagueDashboard from "../components/LeagueDashboard";
+import EplDashboard from "../components/EplDashboard";
 
 const Home = () => {
   const steps = [
@@ -27,11 +31,18 @@ const Home = () => {
   return (
     <div>
       <Layout>
+        <Welcome />
+        <LeagueDashboard />
+        <EplDashboard />
+
         <Wrapper />
         <div className="flex w-[90%] m-auto gap-4 justify-between my-[2rem]">
           {steps.map((step) => {
             return (
-              <aside className="flex bg-[#19222B] w-[400px] p-6 justify-between" key={step.Id}>
+              <aside
+                className="flex bg-[#19222B] w-[400px] p-6 justify-between"
+                key={step.Id}
+              >
                 <div className="w-[86px] h-[86px] text-[#5176D6] border-[1px] border-[#5176D6] bg-[#1D293D] border-dotted rounded-full flex items-center justify-center">
                   0{step.Id}
                 </div>
